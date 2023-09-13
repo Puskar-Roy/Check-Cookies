627b8e012f3415e40ba23514d2856da1d3084ba6
Puskar-Roy <puskarroy600@gmail.com>
2023-09-13 09:29:51 +0530

    Chnage Origin

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://cookiess.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })

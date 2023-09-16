78aafdfab9c37cbef1785a84ceeaa5b51d7c8786
Puskar-Roy <puskarroy600@gmail.com>
2023-09-16 20:36:16 +0530

    Change : Origin

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://cookiess.netlify.app"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
